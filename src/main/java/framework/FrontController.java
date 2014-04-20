04de8c3ca30ba1e67ff084be8e5a32d1d27537ec
astomusic <astomusic@naver.com>
2014-04-20 22:29:28 +0900

    close #43 java servlet에 org.apache.commons.fileupload를 추가하여 formdata 처리

--- src/main/java/framework/FrontController.java
+++ src/main/java/framework/FrontController.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,17 +14,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
 import type.UserProfile;
 import database.DatabaseByMysql;
 import database.DatabaseController;
 
 public class FrontController extends HttpServlet {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	HashMap<String, String> map = new HashMap<String, String>();
 	DatabaseController dbc;
 	JsonBuilder jb;
@@ -32,65 +40,106 @@ public class FrontController extends HttpServlet {
 		super.init();
 		map.put("/test", "/test.jsp");
 		map.put("/index", "/index.jsp");
-		
+
 		dbc = new DatabaseByMysql();
 		jb = new JsonBuilder();
 	}
-	
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
 		String path = request.getServletPath();
 		String result = map.get(path);
 		response.setCharacterEncoding("utf8");
 		request.setCharacterEncoding("utf8");
-		
+
 		if (result != null) {
-			RequestDispatcher dispather = getServletContext().getRequestDispatcher(result);
+			RequestDispatcher dispather = getServletContext()
+					.getRequestDispatcher(result);
 			dispather.forward(request, response);
-		} 
-		else {
+		} else {
 			request.setAttribute("error", "존재하지 않는 주소입니다");
-			RequestDispatcher dispather = getServletContext().getRequestDispatcher("/error.jsp");
+			RequestDispatcher dispather = getServletContext()
+					.getRequestDispatcher("/error.jsp");
 			dispather.forward(request, response);
 		}
 
-		
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		String path = request.getServletPath();
-		
-		if(path.equals("/test")) {
+
+		if (path.equals("/test")) {
 			test(request, response);
 		}
-			
-		//super.doPost(request, response);
+
+		// super.doPost(request, response);
 	}
-	
-	private void test(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	private void test(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		String path = request.getServletPath();
 		String result = map.get(path);
+		String id = null;
 		System.out.println(request.getHeader("Content-type"));
-		String id = (String) request.getParameter("id");
-		//Part idx = request.getPart("id");
-		System.out.println(id);
-		
-		UserProfile dut = dbc.readtable("USER_PROFILE", id);
-		String json = jb.javaToJson(dut);
-		System.out.println(json);
-		response.setCharacterEncoding("utf8");
-		request.setCharacterEncoding("utf8");
-		
-		request.setAttribute("json", json);
 
-		if (result != null) {
-			RequestDispatcher dispather = getServletContext().getRequestDispatcher(result);
+		boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+		if (isMultipart) {
+			FileItemFactory factory = new DiskFileItemFactory();
+			ServletFileUpload upload = new ServletFileUpload(factory);
+			List items = null;
+			try {
+				items = upload.parseRequest(request);
+			} catch (FileUploadException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			Iterator ir = items.iterator();
+			while (ir.hasNext()) {
+		        FileItem item = (FileItem) ir.next();
+
+		        if (item.isFormField()) {
+		            // Process form field.
+		            String name = item.getFieldName();
+		            String value = item.getString();
+		            id = value;
+		            System.out.println(name + value);
+		        } else {
+		            // Process uploaded file.
+		            //String fieldName = item.getFieldName();
+		            //String fileName = item.getName();
+		            //String contentType = item.getContentType();
+		            //boolean isInMemory = item.isInMemory();
+		            //long sizeInBytes = item.getSize();
+		        }
+		    }
+			
+		} else {
+			id = (String) request.getParameter("id");
+		}
+
+		System.out.println(id);
+		if(!id.isEmpty()) {
+			UserProfile dut = dbc.readtable("USER_PROFILE", id);
+			String json = jb.javaToJson(dut);
+			System.out.println(json);
+			response.setCharacterEncoding("utf8");
+			request.setCharacterEncoding("utf8");
+			request.setAttribute("json", json);
+		} else {
+			System.out.println("id is null");
+		}
+		
+		if (!result.isEmpty()) {
+			RequestDispatcher dispather = getServletContext()
+					.getRequestDispatcher(result);
 			dispather.forward(request, response);
 		} else {
 			System.out.println("path error");
 			System.out.println(path);
 			System.out.println(result);
 		}
-	}	
+	}
 }
--- src/main/webapp/javascripts/main.js
+++ src/main/webapp/javascripts/main.js
@@ -7,7 +7,9 @@ function requestSearch(e){
 	var id = e.target.parentElement[0].value;
 	console.log(id);
 	var url = "/test";
-	
+	var update = document.querySelectorAll("#profile-detail-article p");
+	//console.log(update);
+
 	var request = new XMLHttpRequest();
 	request.open("POST", url, true);
 	//request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -16,7 +18,11 @@ function requestSearch(e){
 	request.send(formdata); 
 	request.onreadystatechange = function(){
 		if(request.readyState == 4 && request.status == 200){
-			console.log(request);
+			console.log(request.response);
+			var jsonObj = JSON.parse(request.response)
+			update[0].innerHTML = "profilePhone : " + jsonObj.profilePhone;
+			update[1].innerHTML = "profileStatus : " + jsonObj.profileStatus;
+			update[2].innerHTML = "profileVerification : " + jsonObj.profileVerification;
 			//document.location.reload(true);
 		}
 	}
