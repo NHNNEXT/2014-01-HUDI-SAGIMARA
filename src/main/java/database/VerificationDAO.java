ae840573a6956ce2822fbf34173cf2f521d66a82
sunghwan <namul10@gmail.com>
2014-05-25 02:18:51 +0900

    VerficiationDAO 인덴트 수

--- src/main/java/database/VerificationDAO.java
+++ src/main/java/database/VerificationDAO.java
@@ -8,24 +8,26 @@ import java.sql.SQLException;
 import dto.Verification;
 
 public class VerificationDAO {
-		private Connection conn;
+	private Connection conn;
 
-		public VerificationDAO(Connection conn) {
-			this.conn = conn;
-		}
+	public VerificationDAO(Connection conn) {
+		this.conn = conn;
+	}
 
-		public Verification selectById(String id) throws SQLException {
-			String sql = "select * from " + "VERIFICATION" + " where USER_user_phone = ?";
-			PreparedStatement pstmt;
-			ResultSet rs;
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, id);
-			rs = pstmt.executeQuery();
-			
-			Verification verification = null;
-			if(rs.next()) {
-				 verification = new Verification(rs.getString("USER_user_phone"), rs.getString("verification_time")); 
-			}
-			return verification;
+	public Verification selectById(String userPhone) throws SQLException {
+		String sql = "select * from " + "VERIFICATION"
+				+ " where USER_user_phone = ?";
+		PreparedStatement pstmt;
+		ResultSet rs;
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, userPhone);
+		rs = pstmt.executeQuery();
+
+		Verification verification = null;
+		if (rs.next()) {
+			verification = new Verification(rs.getString("USER_user_phone"),
+					rs.getString("verification_time"));
 		}
+		return verification;
+	}
 }
