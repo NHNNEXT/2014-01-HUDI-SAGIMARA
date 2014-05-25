550794d8b50354a940064d1ce5c91af9e0a2264f
astomusic <astomusic@naver.com>
2014-05-25 15:00:44 +0900

    #116 DatabaseManager의 add메소드 DAO로 분리 (inquiry, location,video, request) admin 로직 사용하지 않는데 어떻게 처리해야하는건지(확인 with 병우)

---
+++ src/main/java/database/InquiryDAO.java
@@ -0,0 +1,41 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import logger.SagimaraLogger;
+
+import org.apache.log4j.Logger;
+
+import dto.Inquiry;
+
+public class InquiryDAO {
+	private Connection conn;
+	Logger logger = SagimaraLogger.logger;
+
+	public InquiryDAO(Connection conn) {
+		this.conn = conn;
+	}
+	
+	public void add(Inquiry inquiry) throws SQLException {
+		String tableName = inquiry.getTableName();
+
+		String sql = "INSERT INTO " + tableName
+				+ " (`USER_user_phone`,`inquiry_time`) VALUES (?, ?)";
+		
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, inquiry.getInquiryId());
+		pstmt.setString(2, inquiry.getInquiryTime());
+
+		int result = pstmt.executeUpdate();
+		
+		if (result == 1) {
+			logger.info("Add Complete " + tableName + " : " + inquiry.getInquiryId() + "," + inquiry.getInquiryTime());
+		} else {
+			logger.info("Add Fail " + tableName);
+		}
+		
+		pstmt.close();
+	}
+}
--- src/main/java/database/UserInquiryDAO.java
+++ src/main/java/database/UserInquiryDAO.java
@@ -16,15 +16,19 @@ public class UserInquiryDAO {
 	
 	public UserInquiry selectById(String id) throws SQLException {
 		String sql = "select * from " + "USER_INQUIRY" + " where phone_number = ?";
-		PreparedStatement pstmt;
-		ResultSet rs;
-		pstmt = conn.prepareStatement(sql);
+		PreparedStatement pstmt = conn.prepareStatement(sql);
 		pstmt.setString(1, id);
-		rs = pstmt.executeQuery();
+		ResultSet rs = pstmt.executeQuery();
+		
 		UserInquiry userInquiry = null;
+		
 		if(rs.next()) {
 			userInquiry = new UserInquiry(rs.getString("phone_number"), rs.getString("today"),rs.getString("1day ago"), rs.getString("2day ago"),rs.getString("3day ago"), rs.getString("4day ago"));
 		}
+		
+		pstmt.close();
+		rs.close();
+
 		return userInquiry;
 	}
 }
