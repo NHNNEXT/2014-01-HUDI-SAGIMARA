a33502f3e439a156fc65f3a5d3e2737a8a1a2c16
astomusic <astomusic@naver.com>
2014-05-29 11:44:00 +0900

    에러난거 푸쉬하지 말기

--- src/main/java/controller/RequestMapping.java
+++ src/main/java/controller/RequestMapping.java
@@ -21,7 +21,7 @@ public class RequestMapping {
 		put("/insert/location", new ForwardController("/insertLocation.jsp"));
 		put("/admin/login", new ForwardController("/admin_login.jsp"));
 		put("/main_test", new ForwardController("/main_test.jsp"));
-		put("/admin/index",new AdminIndexPageController("/admin_index.jsp"));
+		//put("/admin/index",new AdminIndexPageController("/admin_index.jsp"));
 		
 	}};
 	
