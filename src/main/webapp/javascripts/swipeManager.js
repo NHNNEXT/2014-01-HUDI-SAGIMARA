8ed4f7b4a88f79867a8eb8117087708f72c1a7b7
sunsidew <danielay@naver.com>
2014-06-11 19:58:42 +0900

    3a09e0a(#160) 리팩토링 일부 작업 완료 - commentline 38,50,65

--- src/main/webapp/javascripts/swipeManager.js
+++ src/main/webapp/javascripts/swipeManager.js
@@ -8,6 +8,14 @@ var screenX = window.innerWidth;
 var validLength = screenX/5;
 
 var swipeManager = {
+	
+	setTransition : function(index,styleContent) {
+		swiperItem[index].style.Transition = styleContent;
+		swiperItem[index].style.WebkitTransition = styleContent;
+		swiperItem[index].style.MozTransition = styleContent;
+		swiperItem[index].style.MsTransition = styleContent;
+	},
+
 	swipeCheck : function(touchPos, startPos) {
 		if(touchPos-startPos > validLength) { // right swipe action
 			return -1;
@@ -27,20 +35,21 @@ var swipeManager = {
 		];
 
 		for(i = 0 ; i < swiperItem.length ; i++) {
-			//swiperItem[i].style.left = 100*i+"%";
 			ItemX[i] = 100*i;
 		}
-		//timer = setInterval(update, 15);
+		
+		var swipeCheck = this.swipeCheck;
+		var setTransition = this.setTransition;
 
 		swiper.addEventListener('touchend', function() {
 			for(i = 0 ; i < swiperItem.length ; i++) {
 				console.log("bef : "+swiperItem[i].style.left);
 				var leftValue = parseFloat(swiperItem[i].style.left);
-				if(swipeManager.swipeCheck(touchX, startTouchX) > 0)
+				if(swipeCheck(touchX, startTouchX) > 0)
 				{
 					leftValue = Math.floor(leftValue/100)*100;
 				}
-				else if(swipeManager.swipeCheck(touchX, startTouchX) < 0)
+				else if(swipeCheck(touchX, startTouchX) < 0)
 				{
 					leftValue = Math.ceil(leftValue/100)*100;
 				}
@@ -49,16 +58,16 @@ var swipeManager = {
 					leftValue = Math.round(leftValue/100)*100;
 				}
 				//-webkit-transition: left 1s; -transition: left 1s;
-				swiperItem[i].style.Transition = 'left 0.5s';
-				swiperItem[i].style.WebkitTransition = 'left 0.5s';
+				setTransition(i,'left 0.5s');
+				
 				swiperItem[i].style.left = leftValue + "%";
+				
 				ItemX[i] = leftValue;
 				console.log("0.5s");
 				
 				setTimeout(function(){
 					for(i = 0 ; i < swiperItem.length ; i++) {
-						swiperItem[i].style.Transition = 'left 0s';
-						swiperItem[i].style.WebkitTransition = 'left 0s';
+						setTransition(i,'left 0s');
 						swiperItem[i].style.left = (ItemX[i] == 0 ? 0 : -100) + "%";
 						console.log("0s");
 			  		}}
@@ -77,8 +86,7 @@ var swipeManager = {
 		  var movePercent = (touchX-startTouchX)/screenX*100+startPercent;
 		  if(movePercent <= 0 && movePercent >= -100*(swiperItem.length-1)) {
 		  	for(i = 0 ; i < swiperItem.length ; i++) {
-		  		swiperItem[i].style.Transition = 'left 0s';
-				swiperItem[i].style.WebkitTransition = 'left 0s';
+		  		setTransition(i,'left 0s');
 		  		swiperItem[i].style.left = (movePercent+i*100) + "%";
 		  	}
 		  }
